145f7a06c4e5637cd78a5d299cc3abc6f112d078
MariyaMurova <72043153+MariyaMurova@users.noreply.github.com>
2020-09-29 07:27:44 +0300

    updated

--- cneschool-app/src/store/index.js
+++ cneschool-app/src/store/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import state from './state'
+import getters from './getters'
+import mutations from './mutations'
+import actions from './actions'
+
 Vue.use(Vuex)
 
+// import moduleAuth from './auth/moduleAuth.js'
+import moduleECommerce from './eCommerce/moduleECommerce.js'
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  getters,
+  mutations,
+  state,
+  actions,
   modules: {
+    // auth: moduleAuth,
+    eCommerce: moduleECommerce
   }
 })
