db46a90b8af0cde672a9d5f3d070e0ecffebc3b6
jinziqiang58 <ziqiangj6@gmail.com>
2020-12-17 13:27:28 +0300

    updated

--- cneschool-app/vue.config.js
+++ cneschool-app/vue.config.js
@@ -1,5 +1,27 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'git.eschool.ele',
+        nsis:{
+          shortcutName:'Eschool 1.0'
+        },
+        win:{
+          target:[
+            {
+              target: "nsis",
+              arch:[
+                'x64',
+                'ia32'
+              ]
+            }
+          ]
+        }
+        // options placed here will be merged with default configuration and passed to electron-builder
+      }
+    }
+  }
 }
