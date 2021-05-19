f3ad6b00dc6c940a71c6c6b34fd638059a02f6f1
jinziqiang58 <ziqiangj6@gmail.com>
2021-05-19 18:20:23 +0800

    updated for mac

--- cneschool-app/vue.config.js
+++ cneschool-app/vue.config.js
@@ -3,23 +3,31 @@ module.exports = {
     "vuetify"
   ],
   pluginOptions: {
+    // externals: ['adm-zip']
     electronBuilder: {
       builderOptions: {
         appId: 'git.eschool.ele',
         nsis:{
           shortcutName:'Eschool 1.30'
         },
-        win:{
+        mac:{
           target:[
             {
-              target: "nsis",
-              arch:[
-                'x64',
-                'ia32'
-              ]
+              target: "default"
             }
           ]
-        }
+        },
+        // win:{
+        //   target:[
+        //     {
+        //       target: "nsis",
+        //       arch:[
+        //         'x64',
+        //         'ia32'
+        //       ]
+        //     }
+        //   ]
+        // }
         // options placed here will be merged with default configuration and passed to electron-builder
       }
     }
